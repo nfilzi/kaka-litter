c9e28e1b3483830b8f0186632c85c88240292f64
Nicolas <nfilzi.webservices@gmail.com>
2016-10-26 12:47:12 +0200

    Add users table

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20161024165511) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
