321fe3c4226c2ccdcc3fdb1c9fa7923075244c58
Nicolas <nfilzi.webservices@gmail.com>
2016-10-28 23:08:43 +0200

    Associations for users and companies

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :companies
   validates :first_name, :last_name, :phone_number, presence: true
 end
