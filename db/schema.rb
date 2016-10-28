e312868fe2c3bd07454f737acbf57c1dc877734f
Nicolas <nfilzi.webservices@gmail.com>
2016-10-28 23:08:29 +0200

    Add migration for companies + seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024165511) do
+ActiveRecord::Schema.define(version: 20161028204934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "name"
+    t.string  "billing_address"
+    t.string  "phone_number"
+    t.string  "vat_number"
+    t.string  "salesman_email"
+    t.index ["user_id"], name: "index_companies_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 20161024165511) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "companies", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(first_name: "Nicolas", last_name: "Filzi", email: "nfilzi.webservices@gmail.com", password: "password", phone_number: "+33678380989")
+Company.create!(name: "Filzi Company", billing_address: "4 place Charles Fillion", phone_number: "+33678380989", vat_number: "08028402824022", user: User.first)
