4393c78c26f84315eda060dbbb7c2d2cb1089bc7
Nicolas <nfilzi.webservices@gmail.com>
2016-10-29 01:41:10 +0200

    Add static components page
    @Pariente, à toi de jouer 🦄

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :static_components]
 
   def home
+  end
+
+  def static_components
   end
 end
