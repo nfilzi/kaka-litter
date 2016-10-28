abb1934f12706e49d1762c1bd00cd4d59d0a7820
Nicolas <nfilzi.webservices@gmail.com>
2016-10-29 01:17:53 +0200

    Add associations between product, order and order_detail
    + validation on product, order and quantity for order_detail

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,16 @@ class Order < ApplicationRecord
   as_enum :paying_method, [:card, :cash, :cheque, :wire_transfer], source: :paying_method, map: :string, prefix: 'payed_with'
   
   belongs_to :company
+  has_many :order_details
+
   delegate :user, to: :company, allow_nil: true
   
   validates :paying_method, :company, :shipping_address, :total_price_ht, presence: true
+  
+
+  def compute_total_price_ht!
+    self.total_price_ht = order_details.reduce(0) do |total, order_detail| 
+      total += order_detail.quantity * order_detail.product.unit_price
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   serialize :features
-
+  has_many :order_details
+  has_many :orders, through: :order_details
+  
+  
   validates :designation, :unit_price, :features, presence: true
 end
