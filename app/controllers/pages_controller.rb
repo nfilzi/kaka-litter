b529dca8b2c0ed352ac695e3e47996eee73ea016
Nicolas <nfilzi.webservices@gmail.com>
2016-12-20 16:05:56 +0100

    Cleanup pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,16 +4,6 @@ class PagesController < ApplicationController
   def home
     @products = Product.all
   end
-
-  def static_components
-  end
-
-  def test_api_google_drive
-    session = GoogleDrive::Session.from_config(ENV['GOOGLE_DRIVE_JSON_FILEPATH'])
-    spd = session.create_spreadsheet("hello_world")
-    p spd
-    raise
-  end
 end
 
 
