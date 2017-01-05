7dc0e7232f92a3e2eb342efb8958c256d4f23e2c
Nicolas <nfilzi.webservices@gmail.com>
2017-01-05 12:14:40 +0100

    Add normalized designation to get product images correctly

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,10 @@ class Product < ApplicationRecord
   
   
   validates :designation, :unit_price, :features, presence: true
+
+  # FIXME: put that in a decorator, but draper is broken
+  # Need to learn how to build my own :D
+  def normalized_designation
+    designation.downcase.gsub(" ", "_")
+  end
 end
