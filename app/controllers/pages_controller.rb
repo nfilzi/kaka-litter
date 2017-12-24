a36978ff9e7eaa0a76d5c45baaa4971cea9740f4
pariente <parientethomas@gmail.com>
2017-12-24 12:57:28 +0100

    added prices below product pics on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :legal, :static_components]
 
+  def home
+    @box = Product.find(1)
+    @display_box = Product.find(2)
+  end
+
   def shop
     @products = Product.all
   end
