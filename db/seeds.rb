9632135e461ae3055a86de9345d1e9d2e4589788
Nicolas Filzi <nfilzi.webservices@gmail.com>
2018-01-07 21:52:45 +0200

    Fix seeds with correct prices

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Product.destroy_all
 
 puts "Creating products from Kaka Litter's Catalog..."
-litter_bag = Product.create!(sku: "litter-box", designation: "Box of 176 oz", unit_price: 3.4, features: ["Soft & light", "Stays dry on the surface", "Box with pouring spout"])
-box_of_six = Product.create!(sku: "display-box", designation: "Display-box of 6 boxes", unit_price: 19.0)
+litter_bag = Product.create!(sku: "litter-box", designation: "Box of 176 oz", unit_price: 10, features: ["Soft & light", "Stays dry on the surface", "Box with pouring spout"])
+box_of_six = Product.create!(sku: "display-box", designation: "Display-box of 6 boxes", unit_price: 50)
